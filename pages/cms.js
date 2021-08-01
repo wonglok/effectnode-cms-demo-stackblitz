c0cf5cefa7e9f57c49528a6bd3bc73e007358d99
wonglok <yellowhappy831@gmail.com>
2021-08-01 09:52:42 +0800

    3 files modified

--- pages/cms.js
+++ pages/cms.js
@@ -1,8 +1,6 @@
-import { CMSOnePage } from 'effectnode-cms';
+import { CMSOnePage } from 'effectnode-cms/modern.js';
 import { getCodes, firebaseConfig } from '../vfx';
 
 export default function OnePageDemo() {
   return <CMSOnePage firebaseConfig={firebaseConfig} codes={getCodes()} />;
 }
-
-//
