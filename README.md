4e0cc9d8e8976355d9b8fdefd33108d66a1cae8c
wonglok <yellowhappy831@gmail.com>
2021-08-01 16:26:27 +0800

    save

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # EffectNode VFX CMS Starter
 
 ![EffectNode VFX CMS Starter](https://github.com/wonglok/effectnode-cms-starter/blob/master/public/img/cms-demo.png?raw=true)
+
+![EffectNode Dream Code API](https://github.com/wonglok/effectnode-cms-starter/blob/master/public/img/dream-code.png?raw=true)
--- pages/cms.js
+++ pages/cms.js
@@ -1,5 +1,5 @@
-import { CMSOnePage } from 'effectnode-cms';
-import { getCodes, firebaseConfig } from '../vfx';
+import { CMSOnePage } from "effectnode-cms";
+import { getCodes, firebaseConfig } from "../vfx";
 
 export default function OnePageDemo() {
   return <CMSOnePage firebaseConfig={firebaseConfig} codes={getCodes()} />;
